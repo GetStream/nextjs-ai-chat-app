04fa59409c7906a17621500962607a1ef7791a15
Stefan <stefan.blos@gmail.com>
2024-10-16 22:10:06 +0200

    Input UI

--- components/MyMessageInput/MyMessageInput.tsx
+++ components/MyMessageInput/MyMessageInput.tsx
@@ -1,24 +1,23 @@
-import {
-  AttachmentPreviewList,
-  ChatAutoComplete,
-  FileUploadIcon,
-  useMessageInputContext,
-} from 'stream-chat-react';
+import { useMessageInputContext } from 'stream-chat-react';
+import ArrowUp from '../Icons/ArrowUp';
 
 export default function MyMessageInput() {
   const { text, handleChange, handleSubmit } = useMessageInputContext();
   return (
-    <div className='flex items-center gap-4 w-full px-4 py-2 bg-background-gray border-t-2 border-border-gray rounded-md text-primary-text'>
-      {/* <AttachmentPreviewList />
-      <FileUploadIcon /> */}
+    <div className='flex items-center gap-4 px-4 py-2 bg-background-gray border-2 border-border-gray rounded-md text-primary-text m-8'>
       <textarea
         value={text}
         className='bg-background-gray text-primary-text flex-1 resize-none h-10 flex items-center p-2'
         onChange={handleChange}
-        placeholder={'Type something...'}
+        placeholder={'Type to ask the AI...'}
         rows={1}
       />
-      <button onClick={handleSubmit}>Send</button>
+      <button
+        onClick={handleSubmit}
+        className='bg-button-primary p-2 rounded-lg font-semibold'
+      >
+        <ArrowUp />
+      </button>
     </div>
   );
 }
